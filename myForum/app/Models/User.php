e60c37b33d27bb6d9c036eeb5c1b57d3b9192181
dbs <1147295446@qq.com>
2017-09-17 17:27:49 +0800

    董必胜原文件代码_0917_PM17:27

---
+++ myForum/app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    public $table = 'users_login';
+
+    public $primaryKey = 'user_id';
+
+    public $guarded = [];
+
+    public $timestamps = false;
+}
---
+++ myForum/resources/views/admin/complain/send2.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test测试</title>
+</head>
+<body>
+    <h1>您好!</h1>
+    <h3>提醒:<br>
+        &nbsp;感谢您对我们的支持,我们已将您的举报成功处理!</h3>
+</body>
+</html>
